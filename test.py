c4b81754421ee88508ca479244bffb10bde5f5cd
Jan Hermann <jan@janhermann.cz>
2015-07-13 19:57:21 +0200

    improve test routine

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
+from __future__ import print_function
 import pymbd
 import json
 import numpy as np
 
 energy = pymbd.main('mbddata.json')
 data = json.load(open('mbddata.json'))
+errors = []
 for key in energy:
     my = energy[key]
     ref = [e['value'] for e in data['energy'] if e['name'] == key][0]
-    assert np.linalg.norm(my-ref) < 1e-10
-print('Success: all energies match')
+    if np.linalg.norm(my-ref) > 1e-10:
+        errors.append((key, ref, my))
+if not errors:
+    print('Success: all energies match')
+else:
+    print('There were some erors:')
+    for key, ref, my in errors:
+        print(key, ref, my)

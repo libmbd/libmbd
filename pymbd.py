cfb416ff35621374e7d56caaa368d8503b4139a3
Jan Hermann <jan@janhermann.cz>
2015-11-05 18:48:19 +0100

    add convenient function for free atoms

--- pymbd.py
+++ pymbd.py
@@ -23,6 +23,11 @@ myid = MPI.COMM_WORLD.Get_rank()
 free_atom_db = json.load((Path(__file__).parent/'free_atoms.json').open())
 
 
+def get_free_atom_data(species):
+    return list(zip(*[(at['alpha_0'], at['C6'], at['R_vdw']) for at in
+                      [free_atom_db[sp] for sp in species]]))
+
+
 def printout(s, each=False):
     if each or myid == 0:
         sys.stdout.write('{}\n'.format(s))

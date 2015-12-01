57c9afd2e1edbd0c36129ec2accb0879a05b4fb9
Jan Hermann <jan@janhermann.cz>
2015-12-01 14:58:18 +0100

    update python interface

--- pymbd.py
+++ pymbd.py
@@ -3,6 +3,8 @@ import json
 from mpi4py import MPI
 import sys
 from pathlib import Path
+from mbd import mbd
+import numpy as np
 
 
 bohr = 0.529177249
@@ -10,6 +12,10 @@ ntasks = MPI.COMM_WORLD.Get_size()
 myid = MPI.COMM_WORLD.Get_rank()
 
 free_atom_db = json.load((Path(__file__).parent/'free_atoms.json').open())
+
+mbd.my_task = myid
+mbd.n_tasks = ntasks
+mbd.init_grid(30)
 
 
 def get_free_atom_data(species):
@@ -25,6 +31,34 @@ def printout(s, each=False):
 def printerr(s, each=False):
     if each or myid == 0:
         sys.stderr.write('{}\n'.format(s))
+
+
+def get_damping(xc):
+    return dict(zip(['ts_d', 'ts_s_r', 'mbd_scs_a', 'mbd_ts_a',
+                     'mbd_ts_erf_beta', 'mbd_ts_fermi_beta', 'mbd_rsscs_a',
+                     'mbd_rsscs_beta'],
+                    mbd.get_damping_parameters(xc)))
+
+
+def scale_hirsh(hirsh, alpha, C6, R_vdw):
+    hirsh = np.array(hirsh)
+    return [np.array(q)*hirsh**factor
+            for q, factor in zip([alpha, C6, R_vdw], [1, 2, 1/3])]
+
+
+def mbd_rsscs(geom, alpha_0, C6, R_vdw, beta, a):
+    xyz = [a.xyz/bohr for a in geom]
+    mode = 'M' if ntasks > 1 else ''
+    alpha_scs_dyn = mbd.run_scs(
+        mode, 'fermi,dip,gg', xyz,
+        mbd.alpha_dynamic_ts_all('C', mbd.n_grid_omega, alpha_0, c6=C6),
+        r_vdw=R_vdw, beta=beta, a=a)
+    C6_scs = mbd.get_c6_from_alpha(alpha_scs_dyn)
+    R_vdw_scs = R_vdw*(alpha_scs_dyn[0]/alpha_0)**(1/3)
+    return mbd.get_single_mbd_energy(mode, 'fermi,dip', xyz,
+                                     alpha_scs_dyn[0],
+                                     mbd.omega_eff(C6_scs, alpha_scs_dyn[0]),
+                                     r_vdw=R_vdw_scs, beta=beta, a=a)[0]
 
 
 class ArrayEncoder(json.JSONEncoder):

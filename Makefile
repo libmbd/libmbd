c837d6157ca95d49dd00d444faf1dbc249743163
Jan Hermann <jh@janhermann.cz>
2016-11-16 00:19:44 -0800

    implement anisotropic gaussian dipole potential

--- Makefile
+++ Makefile
@@ -6,20 +6,26 @@ FFLAGS = -Og -fcheck=all
 F2PY = f2py
 BLDDIR = build
 -include system.mk
-EXTERN = mbd_interface.f90 mbd_helper.f90
 
 all: mbd
 
-mbd: mbd.f90 $(addprefix ${BLDDIR}/,${EXTERN:.f90=.o})
-	@mkdir -p ${BLDDIR}
-	${F2PY} -c --build-dir ${BLDDIR} --fcompiler=${FVENDOR} \
-		   --f90exec=${FC} --f90flags="${FFLAGS}" --compiler=${CVENDOR} \
-		   $(wordlist 2,3,$^) -m $@ ${LDFLAGS} $<
-	rsync -a ${BLDDIR}/*.mod .
+mbd: mbd.f90 $(addprefix $(BLDDIR)/,mbd_interface.o mbd_helper.o)
+	@mkdir -p $(BLDDIR)
+	$(F2PY) -c --build-dir $(BLDDIR) --fcompiler=$(FVENDOR) \
+		   --f90exec=$(FC) --f90flags="$(FFLAGS)" --compiler=$(CVENDOR) \
+		   $(wordlist 2,3,$^) -m $@ $(LDFLAGS) $<
+	rsync -a $(BLDDIR)/*.mod .
 
-${BLDDIR}/%.o: %.f90
-	@mkdir -p ${BLDDIR}
-	${FC} -c -fPIC -J ${BLDDIR} ${FFLAGS} -o $@ $^
+mbd_math: mbd_math.f90 $(addprefix $(BLDDIR)/,mbd.o mbd_interface.o mbd_helper.o)
+	@mkdir -p $(BLDDIR)
+	$(F2PY) -c --build-dir $(BLDDIR) --fcompiler=$(FVENDOR) \
+		   --f90exec=$(FC) --f90flags="$(FFLAGS)" --compiler=$(CVENDOR) \
+		   $(filter-out $<,$^) -m $@ $(LDFLAGS) $<
+	rsync -a $(BLDDIR)/*.mod .
+
+$(BLDDIR)/%.o: %.f90
+	@mkdir -p $(BLDDIR)
+	$(FC) -c -fPIC -J $(BLDDIR) $(FFLAGS) -o $@ $^
 
 clean:
 	rm -f *.mod
---
+++ mbd_math.f90
@@ -0,0 +1,123 @@
+module mbd_math
+
+use mbd, only: invert, pi
+
+implicit none
+
+integer, parameter :: n_pts_coulomb = 50
+real(8), parameter :: L_coulomb = 1.d0
+
+contains
+
+function dip_coulomb_coupled_gauss(R1, R2, K, coul) result(dip)
+    real(8), intent(in) :: R1(3), R2(3), K(6, 6)
+    real(8), intent(out), optional :: coul
+    real(8) :: dip(3, 3)
+
+    real(8), dimension(n_pts_coulomb) :: u, w, x
+    real(8) :: R(6), s
+    integer :: i
+    real(8) :: det_K_plus_U2, coul_u, dot, dist
+    real(8) :: work(6, 6), Ks(6, 6)
+    real(8), dimension(3, 3) :: K11, K12, K22, dip_u
+
+    ! print *, "det(K)", get_det(K)
+    s = get_det(K)**(-1.d0/6)
+    Ks = s*K
+    w(:) = 1.d0/n_pts_coulomb
+    forall (i = 1:n_pts_coulomb) x(i) = w(1)/2+(i-1)*w(1)
+    ! u = x/(1.d0-x)
+    ! w = 1.d0/(1.d0-x)**2*w
+    dist = sqrt(sum((R1-R2)**2))
+    u = log(1.d0/(1.d0-x))*sqrt(s)*L_coulomb/dist
+    w = 1.d0/(1.d0-x)*w*sqrt(s)*L_coulomb/dist
+    R(1:3) = R1/sqrt(s)
+    R(4:6) = R2/sqrt(s)
+    if (present(coul)) coul = 0.d0
+    dip(:, :) = 0.d0
+    do i = 1, n_pts_coulomb
+        work = Ks
+        call add_U2(work, u(i)**2)  ! work is K+U2
+        det_K_plus_U2 = get_det(work)
+        ! call print_matrix('K', K)
+        ! call print_matrix('K+U2', work)
+        call invert(work)  ! work is (K+U2)^-1
+        ! call print_matrix('(K+U2)^-1', work)
+        work = Ks-matmul(Ks, matmul(work, Ks)) ! work is K-K*(K+U2)^-1*K
+        ! call print_matrix('K-K*(K+U2)^-1*K', work)
+        dot = dot_product(R, matmul(work, R))
+        coul_u = 1.d0/sqrt(det_K_plus_U2)*exp(-dot)*w(i)
+        K11 = work(1:3, 1:3)
+        K12 = work(1:3, 4:6)
+        K22 = work(4:6, 4:6)
+        dip_u = (-2*K12+4*get_outer( &
+            matmul(K11, R1)+matmul(K12, R2), &
+            matmul(K12, R1)+matmul(K22, R2) &
+        )/s)*coul_u
+        ! print *, "u =", u(i)**2, "w =", w(i), "det(K+U2) =", det_K_plus_U2, &
+        !     "dot =", dot, "exp =", exp(-dot), "add =", coul_add
+        if (present(coul)) coul = coul + coul_u
+        dip = dip + dip_u
+    end do
+    coul = 2.d0/sqrt(pi)*coul/sqrt(s)
+    dip = 2.d0/sqrt(pi)*dip*s**(-3.d0/2)
+
+    contains
+
+    subroutine  add_U2(A, u_sq)
+        real(8), intent(inout) :: A(6, 6)
+        real(8), intent(in) :: u_sq
+
+        integer :: i
+
+        forall (i = 1:3)
+            A(i, i) = A(i, i) + u_sq
+            A(i, i+3) = A(i, i+3) - u_sq
+            A(i+3, i) = A(i+3, i) - u_sq
+            A(i+3, i+3) = A(i+3, i+3) + u_sq
+        end forall
+    end subroutine
+
+end function
+
+real(8) function get_det(A) result(D)
+    real(8), intent(in) :: A(:, :)
+
+    integer :: n, i, info
+    real(8) :: LU(size(A, 1), size(A, 1))
+    integer :: ipiv(size(A, 1))
+
+    n = size(A, 1)
+    LU = A
+    call DGETRF(n, n, LU, n, ipiv, info)
+    D = product((/ (LU(i, i), i = 1, n) /))
+end function
+
+function get_outer(a, b) result(C)
+    real(8), intent(in) :: a(:), b(:)
+    real(8) :: C(size(a), size(b))
+
+    integer :: i, j
+
+    forall (i = 1:size(a), j = 1:size(b)) C(i, j) = a(i)*b(j)
+end function
+
+subroutine print_matrix(label, A)
+    character(len=*), intent(in) :: label
+    real(8), intent(in) :: A(:, :)
+
+    integer :: m, n, i, j
+
+    m = size(A, 1)
+    n = size(A, 2)
+
+    write (6, '(A,":")') label
+    do i = 1, m
+        do j = 1, n
+            write (6, "(g10.3)", advance="no") A(i, j)
+        end do
+        write (6, *)
+    end do
+end subroutine
+
+end module

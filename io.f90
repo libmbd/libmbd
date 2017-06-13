8d8e7bc97fbaf3c22bbc090dbf5039f153c224eb
nnnnnn.m6@gmail.com <nnnnnn.m6@gmail.com>
2017-06-13 13:42:48 +0200

    io code

--- io.f90
+++ io.f90
@@ -7,12 +7,17 @@ subroutine printmat(A, message)
 implicit none
 double precision, dimension(:, :), intent(in) :: A
 character(len=*), optional :: message
+character(len=20) :: fmt
 integer :: i, n
 
 if(present(message))print*, message
 n = size(A, 1)
+write (fmt, *) n
+fmt = "(1x,"//trim(adjustl(fmt))//"f8.4)"
+!print*, fmt
+!stop
 do i = 1, n
-print*, A(i, :)
+ write(*, trim(fmt)) A(i, :)
 end do
 
 end subroutine printmat

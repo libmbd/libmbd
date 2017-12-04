b22eebcf19ed6ebba3fee6d0272e5b660d991adc
Jan Hermann <dev@hermann.in>
2017-12-05 00:02:53 +0100

    switch to cmake

--- pymbd_build.py
+++ pymbd_build.py
@@ -1,24 +1,10 @@
-from pathlib import Path
-import json
 import cffi
 
-from configure import conf_file
-
-if Path(conf_file).exists():
-    with open(conf_file) as f:
-        blddir = json.load(f)['blddir']
-root = Path(__file__).parent
-
-header = 'mbd.h'
 ffibuilder = cffi.FFI()
 ffibuilder.set_source(
     'pymbd._libmbd',
-    f'#include "{header}"',
-    include_dirs=[str(root/'src')],
+    f'#include "mbd.h"',
     libraries=['mbd'],
-    library_dirs=[blddir],
 )
-ffibuilder.cdef((Path('src')/header).read_text())
-
-if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
+with open('src/mbd.h') as f:
+    ffibuilder.cdef(f.read())

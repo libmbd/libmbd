17b3aeef412aa088fd8b2792988531654be80e68
Jan Hermann <dev@hermann.in>
2017-12-04 02:15:54 +0100

    create c api

---
+++ src/mbd.h
@@ -0,0 +1,24 @@
+// vim: set ft=c:
+
+struct MBD_calc* mbd_init_calc(int n_grid);
+
+void mbd_destroy_calc(struct MBD_calc* calc);
+
+struct MBD_damping* mbd_init_damping(
+    int n_atoms,
+    double* R_vdw,
+    double beta,
+    double a
+);
+
+void mbd_destroy_damping(struct MBD_damping* damping);
+
+void mbd_calculate(
+    struct MBD_calc* calc,
+    int n_atoms,
+    double* coords,
+    double* alpha_0,
+    double* omega,
+    struct MBD_damping* damping,
+    double* energy
+);
---
+++ src/mbd_c_api.f90
@@ -0,0 +1,75 @@
+module mbd_c_api
+
+use iso_c_binding
+use mbd, only: mbd_calc, mbd_damping, get_mbd_energy, init_grid, destroy_grid
+
+implicit none
+
+contains
+
+type(c_ptr) function mbd_init_calc(n_grid) bind(c)
+    integer(c_int), intent(in), value :: n_grid
+
+    type(mbd_calc), pointer :: calc
+
+    allocate (calc)
+    call init_grid(calc, n_grid)
+    mbd_init_calc = c_loc(calc)
+end function mbd_init_calc
+
+subroutine mbd_destroy_calc(calc_p) bind(c)
+    type(c_ptr), value :: calc_p
+
+    type(mbd_calc), pointer :: calc
+
+    call c_f_pointer(calc_p, calc)
+    call destroy_grid(calc)
+    deallocate (calc)
+end subroutine mbd_destroy_calc
+
+type(c_ptr) function mbd_init_damping(n_atoms, r_vdw, beta, a) bind(c)
+    integer(c_int), intent(in), value :: n_atoms
+    real(c_double), intent(in) :: r_vdw(n_atoms)
+    real(c_double), intent(in), value :: beta
+    real(c_double), intent(in), value :: a
+
+    type(mbd_damping), pointer :: damping
+
+    allocate (damping)
+    damping%r_vdw = r_vdw
+    damping%beta = beta
+    damping%a = a
+    mbd_init_damping = c_loc(damping)
+end function mbd_init_damping
+
+subroutine mbd_destroy_damping(damping_p) bind(c)
+    type(c_ptr), value :: damping_p
+
+    type(mbd_damping), pointer :: damping
+
+    call c_f_pointer(damping_p, damping)
+    deallocate (damping%r_vdw)
+    deallocate (damping)
+end subroutine mbd_destroy_damping
+
+subroutine mbd_calculate(calc_p, n_atoms, coords, alpha_0, omega, damping_p, energy) bind(c)
+    type(c_ptr), intent(in), value :: calc_p
+    integer(c_int), intent(in), value :: n_atoms
+    real(c_double), intent(in) :: coords(n_atoms, 3)
+    real(c_double), intent(in) :: alpha_0(n_atoms)
+    real(c_double), intent(in) :: omega(n_atoms)
+    type(c_ptr), intent(in), value :: damping_p
+    real(c_double), intent(out) :: energy
+
+    type(mbd_calc), pointer :: calc
+    type(mbd_damping), pointer :: damping
+
+    call c_f_pointer(calc_p, calc)
+    call c_f_pointer(damping_p, damping)
+    energy = get_mbd_energy( &
+        calc, '', 'fermi,dip', coords, alpha_0, omega, &
+        r_vdw=damping%r_vdw, beta=damping%beta, a=damping%a &
+    )
+end subroutine mbd_calculate
+
+end module mbd_c_api

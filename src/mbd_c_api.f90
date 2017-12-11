3dcbafe5046a493c270a6729fab68c50e1dc6ee0
Jan Hermann <dev@hermann.in>
2017-12-11 19:52:04 +0100

    add missing license header
    [ci skip]

--- src/mbd_c_api.f90
+++ src/mbd_c_api.f90
@@ -1,3 +1,6 @@
+! This Source Code Form is subject to the terms of the Mozilla Public
+! License, v. 2.0. If a copy of the MPL was not distributed with this
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.
 module mbd_c_api
 
 use iso_c_binding, only: c_ptr, c_int, c_double, c_f_pointer, c_loc, c_bool

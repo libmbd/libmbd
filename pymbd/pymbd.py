95ad3357f8a027062aa4e1eb25451a320b07ad92
Jan Hermann <dev@hermann.in>
2017-12-12 14:48:57 +0100

    add general function for calculating numerical forces

--- pymbd/pymbd.py
+++ pymbd/pymbd.py
@@ -85,6 +85,25 @@ class MBDCalc(object):
         return self.mbd_energy(coords, alpha_0, omega, R_vdw, beta, **kwargs)
 
 
+def numerical_forces(f, coords, *args, **kwargs):
+    delta = kwargs.pop('delta', 1e-3)  # support python 2
+    coords = np.array(coords)
+    forces = np.zeros(coords.shape)
+    for i_atom in range(coords.shape[0]):
+        for i_xyz in range(3):
+            ene = {}
+            for step in [-2, -1, 1, 2]:
+                coords_diff = coords.copy()
+                coords_diff[i_atom, i_xyz] += step*delta
+                ene[step] = f(coords_diff, *args, **kwargs)
+            forces[i_atom, i_xyz] = _diff5(ene, delta)
+    return forces
+
+
+def _diff5(x, delta):
+    return (1./12*x[-2]-2./3*x[-1]+2./3*x[1]-1./12*x[2])/delta
+
+
 def _get_vdw_params():
     csv_lines = pkg_resources.resource_string(__name__, 'vdw-params.csv').split(b'\n')
     if sys.version_info[0] > 2:

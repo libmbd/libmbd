1ec3c1e993e4b68866e379b6034cc3480e882625
Jan Hermann <dev@janhermann.cz>
2018-08-29 01:34:06 +0200

    downgrade debug flags for gfortran to 2003 standard

--- cmake/fortran_flags_override.cmake
+++ cmake/fortran_flags_override.cmake
@@ -1,4 +1,4 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_Fortran_FLAGS_DEBUG_INIT
-        "-Og -fcheck=all -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -std=f2008ts -pedantic -fall-intrinsics -Wno-maybe-uninitialized")
+        "-Og -fcheck=all -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -std=f2003 -pedantic -fall-intrinsics -Wno-maybe-uninitialized")
 endif()

b6bff43fd63b84fc3d2b38677d662f6332537109
Jan Hermann <dev@jan.hermann.name>
2019-08-13 16:43:13 +0200

    add 3-point finite differencing

--- pymbd/utils.py
+++ pymbd/utils.py
@@ -10,37 +10,47 @@ __all__ = ()
 
 def numerical_gradients(geom, func, *args, **kwargs):
     delta = kwargs.pop('delta', 1e-3)  # support python 2
+    steps, diff = finite_diff_gen(kwargs.pop('npts', 5))
     coords_0 = geom.coords
     gradients = np.zeros(coords_0.shape)
     for i_atom in range(coords_0.shape[0]):
         for i_xyz in range(3):
             ene = {}
-            for step in [-2, -1, 1, 2]:
+            for step in steps:
                 coords = coords_0.copy()
                 coords[i_atom, i_xyz] += step * delta
                 geom.coords = coords
                 ene[step] = getattr(geom, func)(*args, **kwargs)
-            gradients[i_atom, i_xyz] = _diff5(ene, delta)
+            gradients[i_atom, i_xyz] = diff(ene, delta)
     return gradients
 
 
 def numerical_latt_gradients(geom, func, *args, **kwargs):
     delta = kwargs.pop('delta', 1e-3)
+    steps, diff = finite_diff_gen(kwargs.pop('npts', 5))
     lattice_0 = geom.lattice
     gradients = np.zeros((3, 3))
     for i_vec in range(3):
         for i_xyz in range(3):
             ene = {}
-            for step in [-2, -1, 1, 2]:
+            for step in steps:
                 lattice = lattice_0.copy()
                 lattice[i_vec, i_xyz] += step * delta
                 geom.lattice = lattice
                 ene[step] = getattr(geom, func)(*args, **kwargs)
-            gradients[i_vec, i_xyz] = _diff5(ene, delta)
+            gradients[i_vec, i_xyz] = diff(ene, delta)
     return gradients
+
+
+def _diff3(x, delta):
+    return (-1.0 / 2 * x[-1] + 1.0 / 2 * x[1]) / delta
 
 
 def _diff5(x, delta):
     return (
         1.0 / 12 * x[-2] - 2.0 / 3 * x[-1] + 2.0 / 3 * x[1] - 1.0 / 12 * x[2]
     ) / delta
+
+
+def finite_diff_gen(npts):
+    return {3: ([-1, 1], _diff3), 5: ([-2, -1, 1, 2], _diff5)}[npts]

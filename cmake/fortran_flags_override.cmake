8a338b9d9ac95fe019bf555c7d8a45038391e335
Jan Hermann <dev@jan.hermann.name>
2019-11-05 15:27:14 +0100

    improve default Fortran flags

--- cmake/fortran_flags_override.cmake
+++ cmake/fortran_flags_override.cmake
@@ -1,4 +1,6 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+    set(CMAKE_Fortran_FLAGS_INIT
+        "-fall-intrinsics -std=f2008ts -pedantic -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -Wno-maybe-uninitialized")
     set(CMAKE_Fortran_FLAGS_DEBUG_INIT
-        "-Og -fcheck=all -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -std=f2008ts -pedantic -fall-intrinsics -Wno-maybe-uninitialized")
+        "-Og -fcheck=all")
 endif()

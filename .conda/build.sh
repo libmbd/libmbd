19037f22d44f3d3d91abf1e9f724a318e6db0df1
Jan Hermann <dev@jan.hermann.name>
2020-08-01 23:51:05 +0200

    remove conda-related code

--- .conda/build.sh
+++
@@ -1,8 +0,0 @@
-set -v
-
-mkdir build
-(cd build && cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib)
-make -C build
-make -C build install
-env LIBMBD_PREFIX=$PREFIX poetry build
-$PYTHON -m pip install pymbd -f ./dist --no-deps --ignore-installed --no-cache-dir -vvv

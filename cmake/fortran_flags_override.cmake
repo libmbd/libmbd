0019356b708c2def3f2cdd2eb0902f5b37495649
Jan Hermann <dev@jan.hermann.name>
2020-08-02 23:00:45 +0200

    [dev] remove array temporary warnings in debug build

--- cmake/fortran_flags_override.cmake
+++ cmake/fortran_flags_override.cmake
@@ -2,5 +2,5 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_Fortran_FLAGS_INIT
         "-fall-intrinsics -std=f2008ts -pedantic -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -Wno-maybe-uninitialized")
     set(CMAKE_Fortran_FLAGS_DEBUG_INIT
-        "-Og -fcheck=all")
+        "-Og -fcheck=all -fno-check-array-temporaries")
 endif()

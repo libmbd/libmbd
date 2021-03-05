2c5befd6e5719413d8f841b8cffb7450c214f1eb
Jan Hermann <dev@jan.hermann.name>
2021-03-05 12:01:07 +0100

    add dev tool to generate source dist of Libmbd

---
+++ devtools/source-dist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+VERSION=$(git describe --tags --dirty=.dirty)
+VERSION_FILE=cmake/LibmbdVersionTag.cmake
+SLUG=libmbd-${VERSION}
+echo "set(VERSION_TAG ${VERSION})">${VERSION_FILE}
+mkdir -p dist
+ARCHIVE=dist/${SLUG}.tar.gz
+gtar -vcz -f ${ARCHIVE} \
+    --exclude "*pymbd*" --exclude "__pycache__" --exclude "conftest.py" --exclude ".*" \
+    --transform "s,^,${SLUG}/," CMakeLists.txt cmake src tests
+rm ${VERSION_FILE}
+echo ${ARCHIVE}

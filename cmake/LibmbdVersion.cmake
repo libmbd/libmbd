e130ce879583566f4e2486f6e674f175e70af6b5
Jan Hermann <dev@jan.hermann.name>
2021-11-26 11:29:38 +0100

    add an error message when installing from source tree
    fixes #28

--- cmake/LibmbdVersion.cmake
+++ cmake/LibmbdVersion.cmake
@@ -6,8 +6,14 @@ if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
     message(STATUS "Setting version tag to ${VERSION_TAG} from Git")
-else()
+elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/cmake/LibmbdVersionTag.cmake")
     include(LibmbdVersionTag)
+else()
+    message(FATAL_ERROR
+        "Not in a Git repository and version tag is missing, you most likely "
+        "attempt to install from a copy of the source tree. Obtain the source "
+        "distribution (libmbd-<version>.tar.gz) from a Github release page "
+        "instead.")
 endif()
 
 set(PROJECT_VERSION ${VERSION_TAG})

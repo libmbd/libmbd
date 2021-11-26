0cf86f99836c84b50b677aad9c1d1e146bd46d98
Jan Hermann <dev@jan.hermann.name>
2021-11-26 10:58:07 +0100

    [dev] add LICENSE and README to Libmbd sdist

--- devtools/source-dist.sh
+++ devtools/source-dist.sh
@@ -9,6 +9,6 @@ mkdir -p dist
 ARCHIVE=dist/${SLUG}.tar.gz
 gtar -vcz -f ${ARCHIVE} \
     --exclude "*pymbd*" --exclude "__pycache__" --exclude "conftest.py" --exclude ".*" \
-    --transform "s,^,${SLUG}/," CMakeLists.txt cmake src tests
+    --transform "s,^,${SLUG}/," CMakeLists.txt cmake src tests LICENSE README.md
 rm ${VERSION_FILE}
 echo ${ARCHIVE}

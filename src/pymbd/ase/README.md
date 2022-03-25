0d0c9f42c052eebb1111ab4040c8bff2bbb96c02
Jan Hermann <dev@jan.hermann.name>
2022-03-25 14:29:55 +0000

    TMP cleanup

---
+++ src/pymbd/ase/README.md
@@ -0,0 +1,1 @@
+The ASE calculators were originally implemented in https://github.com/maurergroup/SchNet-vdW and moved under Pymbd for more cohesive maintenance.
--- src/pymbd/ase/mbd.py
+++ src/pymbd/ase/mbd.py
@@ -22,90 +22,72 @@ class MBD(Calculator):
         k_grid=None,
         scheme='VDW',  # 'VDWw' or 'MBD', default is VDW'
         params='TS',  # either TS or TSSURF
-        nfreq=15,
+        n_freq=15,
         beta=0.83,  # PBE default value
         ts_sr=0.94,  # PBE default value
         do_rpa=False,
         **kwargs,
     ):
+        scheme = scheme.upper()
+        params = params.upper()
+        assert scheme in ['VDW', 'MBD']
         self.k_grid = k_grid
-        self.scheme = scheme.upper()
-        self.params = params.upper()
-        self.nfreq = nfreq
+        self.scheme = scheme
+        self.params = params
+        self.n_freq = n_freq
         self.beta = beta
         self.ts_sr = ts_sr
         self.do_rpa = do_rpa
         if self.params == 'TSSURF':
             self.params = 'TSsurf'
-        self.hirshvolrat_is_set = False
         self.hirsh_volrat = None
-        Calculator.__init__(self, restart, label, atoms, **kwargs)
+        super().__init__(restart, label, atoms, **kwargs)
 
     def calculate(self, atoms, properties=('energy',), system_changes=all_changes):
         """Do the calculation."""
-        self.atoms = atoms.copy()
-
-        if all(atoms.get_pbc()):
-            lattice = np.array(atoms.get_cell()) / units.Bohr
-        else:
-            lattice = None
-
-        if not self.hirshvolrat_is_set:
-            self.hirsh_volrat = np.ones(len(atoms), dtype=np.float)
-
-        if self.calculation_required(atoms, properties):
-            Calculator.calculate(self, atoms)
-
-            self.alpha_0, self.C6, self.R_vdw = from_volumes(
-                atoms.get_chemical_symbols(), self.hirsh_volrat, kind=self.params
+        if not self.calculation_required(atoms, properties):
+            return
+        super().calculate(atoms, properties, system_changes)
+        do_force = 'forces' in properties or 'stress' in properties
+        hirsh_volrat = (
+            self.hirsh_volrat
+            if self.hirsh_volrat is not None
+            else np.ones(len(atoms), dtype=np.float)
+        )
+        alpha_0, C6, R_vdw = from_volumes(
+            atoms.get_chemical_symbols(), hirsh_volrat, kind=self.params
+        )
+        geom = MBDGeom(
+            coords=atoms.positions / units.Bohr,
+            lattice=(
+                np.array(atoms.get_cell()) / units.Bohr
+                if all(atoms.get_pbc())
+                else None
+            ),
+            k_grid=self.k_grid,
+            n_freq=self.n_freq,
+            do_rpa=self.do_rpa,
+        )
+        if self.scheme == 'MBD':
+            result = geom.mbd_energy(alpha_0, C6, R_vdw, beta=self.beta, force=do_force)
+        elif self.scheme == 'VDW':
+            result = geom.ts_energy(
+                alpha_0, C6, R_vdw, sR=self.ts_sr, d=20.0, force=do_force
             )
-
-            if 'forces' in properties or 'stress' in properties:
-                do_force = True
-            else:
-                do_force = False
-
-            mbdgeom = MBDGeom(
-                coords=atoms.positions / units.Bohr,
-                lattice=lattice,
-                k_grid=self.k_grid,
-                n_freq=self.nfreq,
-                do_rpa=self.do_rpa,
-            )
-
-            if self.scheme == 'MBD':
-                energy = mbdgeom.mbd_energy(
-                    self.alpha_0, self.C6, self.R_vdw, beta=self.beta, force=do_force
+        self.results['energy'] = self.results['free_energy'] = (
+            result if not do_force else result[0]
+        ) * units.Hartree
+        if do_force:
+            gradients = result[1] * units.Hartree / units.Bohr
+            self.results['forces'] = -gradients
+            if 'stress' in properties and all(self.atoms.get_pbc()):
+                latt_gradients = result[2] * units.Hartree / units.Bohr
+                stress = np.dot(atoms.get_cell(), latt_gradients.transpose()) + np.dot(
+                    atoms.get_positions().transpose(), gradients
                 )
-            elif self.scheme == 'VDW':
-                energy = mbdgeom.ts_energy(
-                    self.alpha_0,
-                    self.C6,
-                    self.R_vdw,
-                    sR=self.ts_sr,
-                    d=20.0,
-                    force=do_force,
-                )
-            else:
-                raise ValueError('mbd: scheme needs to be MBD or VDW')
-
-            if not do_force:
-                self.results['energy'] = energy * units.Hartree
-            else:
-                self.results['energy'] = energy[0] * units.Hartree
-                gradients = energy[1] * units.Hartree / units.Bohr
-                self.results['forces'] = -gradients
-                if 'stress' in properties and all(self.atoms.get_pbc()):
-                    lattgradients = energy[2] * units.Hartree / units.Bohr
-                    stress = np.dot(
-                        atoms.get_cell(),
-                        lattgradients.transpose(),
-                    ) + np.dot(atoms.get_positions().transpose(), gradients)
-                    stress = stress / (atoms.get_volume())
-                    self.results['stress'] = stress
-            self.results['free_energy'] = self.results['energy']
+                stress = stress / (atoms.get_volume())
+                self.results['stress'] = stress
 
     def set_hirshfeld(self, hirsh_volrat):
         """Set Hirshfeld volumes."""
-        self.hirshvolrat_is_set = True
         self.hirsh_volrat = hirsh_volrat

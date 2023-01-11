f99c4bbbd2212957c88dd22d06628138ede05f34
Katya Zossimova <katyazossia@gmail.com>
2023-01-11 13:21:14 +0100

    DEV: molecular polarizability tensors

--- src/pymbd/__init__.py
+++ src/pymbd/__init__.py
@@ -2,10 +2,12 @@ import re
 
 import pkg_resources
 
-from .pymbd import ang, from_volumes, mbd_energy, mbd_energy_species, screening, atomic_polarizability_tensors
+from .pymbd import (ang, from_volumes, mbd_energy, mbd_energy_species,
+                    screening, atomic_polarizability_tensors, molecular_polarizability_tensor)
 
 __all__ = ['mbd_energy', 'mbd_energy_species', 'screening', 'ang',
            'from_volumes', 'atomic_polarizability_tensors', 'molecular_polarizability_tensor']
+
 try:
     __version__ = pkg_resources.get_distribution('pymbd').version
     __version__ = re.split('[.-]', __version__, 3)

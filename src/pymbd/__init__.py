4d7129556f7d3d01db7e10da2e975d4c910239fb
Katya Zossimova <katyazossia@gmail.com>
2023-01-13 13:32:38 +0100

    calculate atomic and molecular polarizability tensors

--- src/pymbd/__init__.py
+++ src/pymbd/__init__.py
@@ -3,10 +3,11 @@ import re
 import pkg_resources
 
 from .pymbd import (ang, from_volumes, mbd_energy, mbd_energy_species,
-                    screening, atomic_polarizability_tensors, molecular_polarizability_tensor)
+                    screening, atomic_polarizabilities, molecular_polarizability)
 
-__all__ = ['mbd_energy', 'mbd_energy_species', 'screening', 'ang',
-           'from_volumes', 'atomic_polarizability_tensors', 'molecular_polarizability_tensor']
+__all__ = ['mbd_energy', 'mbd_energy_species', 'screening',
+           'from_volumes', 'atomic_polarizabilities',
+           'molecular_polarizability', 'ang']
 
 try:
     __version__ = pkg_resources.get_distribution('pymbd').version

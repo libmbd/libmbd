5de626b06dc69c4cc158849564aa457844009ace
Jan Hermann <github@id.jan.hermann.name>
2023-07-28 16:19:52 +0200

    fix flake8 (#52)

--- src/pymbd/__init__.py
+++ src/pymbd/__init__.py
@@ -7,7 +7,7 @@ from .pymbd import ang, from_volumes, mbd_energy, mbd_energy_species, screening
 __all__ = ['mbd_energy', 'mbd_energy_species', 'screening', 'ang', 'from_volumes']
 try:
     __version__ = pkg_resources.get_distribution('pymbd').version
-    __version__ = re.split('[.-]', __version__, 3)
+    __version__ = re.split('[.-]', __version__, maxsplit=3)
     __version__ = (*map(int, __version__[:3]), *__version__[3:])
 except pkg_resources.DistributionNotFound:
     __version__ = None
